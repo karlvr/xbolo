eb5ecbdd388892eb21a9652374335d6fe6100234
C.W. Betts <computers57@hotmail.com>
2016-05-27 15:21:53 -0600

    Add Magma, or the XBolo map editor.

---
+++ Magma/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  Magma
+//
+//  Created by C.W. Betts on 5/27/16.
+//  Copyright © 2016 Robert Chrzanowski. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+@end
---
+++ Magma/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Magma
+//
+//  Created by C.W. Betts on 5/27/16.
+//  Copyright © 2016 Robert Chrzanowski. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
