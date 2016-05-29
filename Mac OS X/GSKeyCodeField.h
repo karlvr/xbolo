a51a6dd8ef43a4429d5b48ee423a8d339098a589
C.W. Betts <computers57@hotmail.com>
2016-05-29 15:03:29 -0600

    More fiddling…

--- Mac OS X/GSKeyCodeField.h
+++ Mac OS X/GSKeyCodeField.h
@@ -6,6 +6,8 @@
 - (instancetype)initWithKeyCode:(unsigned short)aKeyCode NS_DESIGNATED_INITIALIZER;
 @property (nonatomic) unsigned short keyCode;
 - (IBAction)takeKeyValueFrom:(id)sender;
+
+- (instancetype)initWithCoder:(NSCoder *)decoder NS_DESIGNATED_INITIALIZER;
 @end
 
 @interface GSKeyCodeField : NSControl {
