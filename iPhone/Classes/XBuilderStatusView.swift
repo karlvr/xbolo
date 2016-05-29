da485f489850e8bef20196379e3c71a0b2f8bb7a
C.W. Betts <computers57@hotmail.com>
2016-05-29 14:20:27 -0600

    Add more iPhone view classes.

---
+++ iPhone/Classes/XBuilderStatusView.swift
@@ -0,0 +1,60 @@
+//
+//  BuilderStatusView.swift
+//  XBolo
+//
+//  Created by C.W. Betts on 5/28/16.
+//  Copyright © 2016 Robert Chrzanowski. All rights reserved.
+//
+
+import UIKit
+
+class XBuilderStatusView : UIView {
+  @objc(XBuilderStatusViewState) enum State: Int32 {
+    case Ready
+    case Direction
+    case Dead
+  }
+  
+  var state: State = State.Ready {
+    didSet {
+      if state != oldValue {
+        setNeedsDisplay()
+      }
+    }
+  }
+  var direction: CGFloat = 0 {
+    didSet {
+      if direction != oldValue && state == .Direction {
+        setNeedsDisplay()
+      }
+    }
+  }
+  
+  override func drawRect(dirtyRect: CGRect) {
+    switch (state) {
+    case .Direction:
+      let bounds = self.bounds;
+      UIColor.whiteColor().set()
+      let p = UIBezierPath(ovalInRect: CGRect(x: -2, y: -2, width: 4, height: 4))
+      p.moveToPoint(CGPoint(x: bounds.size.width*0.25, y: bounds.size.height*0.125))
+      p.addLineToPoint(CGPoint(x: bounds.size.width*0.5, y: 0.0))
+      p.addLineToPoint(CGPoint(x: bounds.size.width*0.25, y: -bounds.size.height*0.125))
+      p.moveToPoint(.zero)
+      p.addLineToPoint(CGPoint(x: bounds.size.width*0.5, y: 0.0))
+      
+      var t = CGAffineTransformMakeTranslation(bounds.size.width*0.5, bounds.size.height*0.5)
+      t = CGAffineTransformRotate(t, direction)
+      p.applyTransform(t)
+      p.stroke()
+      p.fill()
+      
+    case .Dead:
+      UIColor(patternImage: UIImage(named: "StipplePattern")!).set()
+      UIBezierPath(ovalInRect: bounds).fill()
+      break;
+      
+    case .Ready:
+      break;
+    }
+  }
+}
---
+++ iPhone/Classes/XStatusBar.swift
@@ -0,0 +1,52 @@
+//
+//  XStatusBar.swift
+//  XBolo
+//
+//  Created by C.W. Betts on 5/29/16.
+//  Copyright © 2016 Robert Chrzanowski. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class XStatusBar: UIView {
+  
+  @objc(XStatusBarType) enum StatusBarType: Int {
+    case Horizontal
+    case Vertical
+  }
+  
+  @IBInspectable
+  var barType: StatusBarType = StatusBarType.Horizontal {
+    didSet {
+      if oldValue != barType {
+        setNeedsDisplay()
+      }
+    }
+  }
+  
+  @IBInspectable
+  var value: CGFloat = 0 {
+    didSet {
+      if oldValue != value {
+        setNeedsDisplay()
+      }
+    }
+  }
+  
+  // Only override drawRect: if you perform custom drawing.
+  // An empty implementation adversely affects performance during animation.
+  override func drawRect(rect: CGRect) {
+    switch barType {
+    case .Horizontal:
+      UIColor.greenColor().set()
+      let path = UIBezierPath(rect: CGRect(x: 0.0, y: 0.0, width: self.frame.width * value, height: self.frame.height))
+      path.fill()
+      
+    case .Vertical:
+      UIColor.greenColor().set()
+      let path = UIBezierPath(rect: CGRect(x: 0.0, y: 0.0, width: self.frame.width, height: self.frame.height * value))
+      path.fill()
+    }
+  }
+}
