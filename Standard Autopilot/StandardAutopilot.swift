ee20e0f3f8a871dd317104b40742ada6a13a2a59
C.W. Betts <computers57@hotmail.com>
2018-06-11 08:04:42 -0600

    Xcode project clean-up. Fix the standard autopilot.

--- Standard Autopilot/StandardAutopilot.swift
+++ Standard Autopilot/StandardAutopilot.swift
@@ -13,14 +13,14 @@ public class StandardAutopilot: NSObject, GSRobotProtocol {
 		super.init()
 	}
 	
-	public static func minimumRobotInterfaceVersionRequired() -> Int32 {
+	public static var minimumRobotInterfaceVersionRequired: Int32 {
 		return GS_ROBOT_CURRENT_INTERFACE_VERSION
 	}
 	
-	public func stepXBoloRobotWithGameState(gameState: UnsafePointer<GSRobotGameState>, freeFunction freeF: (@convention(c) (UnsafeMutablePointer<Void>) -> Void)!, freeContext freeCtx: UnsafeMutablePointer<Void>) -> GSRobotCommandState {
+	public func stepXBoloRobot(with gameState: UnsafePointer<GSRobotGameState>!, freeFunction freeF: (@convention(c) (UnsafeMutableRawPointer?) -> Void)!, freeContext freeCtx: UnsafeMutableRawPointer!) -> GSRobotCommandState {
 		var state = GSRobotCommandState()
 		//let messages = gameState.memory.messages?.takeUnretainedValue() as? NSArray as? [String]
-		let tanks = UnsafeBufferPointer(start: gameState.memory.tanks, count: Int(gameState.memory.tankscount))
+		let tanks = UnsafeBufferPointer(start: gameState.pointee.tanks, count: Int(gameState.pointee.tankscount))
 		//let shells = UnsafeBufferPointer(start: gameState.memory.shells, count: Int(gameState.memory.shellscount))
 		//let builders = UnsafeBufferPointer(start: gameState.memory.builders, count: Int(gameState.memory.builderscount))
 		var targetTank: Tank?
