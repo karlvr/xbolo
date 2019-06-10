19d19de6a6a44462675e88bc90950bc567853715
Karl von Randow <karl@xk72.com>
2019-06-10 22:48:50 +1200

    iPad: use ARC

--- iPhone/Classes/XBoloAppDelegate.m
+++ iPhone/Classes/XBoloAppDelegate.m
@@ -17,20 +17,11 @@ NSString *const GSXBoloErrorDomain = @"GSXBoloErrorDomain";
 @synthesize window;
 @synthesize viewController;
 
-
 - (void)applicationDidFinishLaunching:(UIApplication *)application {    
     
     // Override point for customization after app launch    
     [window addSubview:viewController.view];
     [window makeKeyAndVisible];
 }
-
-
-- (void)dealloc {
-    [viewController release];
-    [window release];
-    [super dealloc];
-}
-
 
 @end
